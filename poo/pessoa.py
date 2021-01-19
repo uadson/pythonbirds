e945bfba7d2432e7756cd156238e79539c06e4f3
uadson <uadsoncastro@gmail.com>
2021-01-19 11:39:06 -0300

    create method

--- poo/pessoa.py
+++ poo/pessoa.py
@@ -1,3 +1,15 @@
 class Pessoa:
-	pass
+	def cumprimentar(self):
+		return f'Olá {id(self)}'
 
+
+if __name__ == '__main__':
+	p = Pessoa()
+	print(Pessoa.cumprimentar(p))
+	# id
+	print(id(p))
+	# ou
+	print(p.cumprimentar())
+
+
+ 
