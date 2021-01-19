bae1764cab71d32f14118256c108745482d23f18
uadson <uadsoncastro@gmail.com>
2021-01-19 11:46:55 -0300

    criado atributo de instancia

--- poo/pessoa.py
+++ poo/pessoa.py
@@ -1,15 +1,24 @@
 class Pessoa:
+	def __init__(self, nome=None, idade=37):
+		self.nome = nome
+		self.idade = idade
+
 	def cumprimentar(self):
 		return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-	p = Pessoa()
+	p = Pessoa('Feitosa')
 	print(Pessoa.cumprimentar(p))
 	# id
 	print(id(p))
 	# ou
 	print(p.cumprimentar())
+	print(p.nome)
+	# alterando o atributo
+	p.nome = 'Uadson'
+	print(p.nome)
+	print(p.idade)
 
 
  
